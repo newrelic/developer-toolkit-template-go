32552495e72ced03ff7b4fc929db6c1803d96ff8
Jonathan Thurman <jthurman@newrelic.com>
2021-12-27 18:46:16 -0800

    chore: update default tools

--- tools/main.go
+++ tools/main.go
@@ -1,23 +1,31 @@
+//go:build tools
 // +build tools
 
 package tools
 
 import (
-	// build/test.mk
+	// Testing
+	_ "github.com/golang/mock/mockgen"
+	_ "github.com/onsi/ginkgo/ginkgo"
 	_ "github.com/stretchr/testify/assert"
 	_ "gotest.tools/gotestsum"
 
-	// build/lint.mk
+	// Linting
 	_ "github.com/client9/misspell/cmd/misspell"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/llorllale/go-gitlint/cmd/go-gitlint"
 	_ "github.com/psampaz/go-mod-outdated"
 	_ "golang.org/x/tools/cmd/goimports"
 
-	// build/document.mk
-	_ "github.com/git-chglog/git-chglog/cmd/git-chglog"
+	// Documentation
 	_ "golang.org/x/tools/cmd/godoc"
 
-	// build/release.mk
+	// Releasing
+	_ "github.com/caarlos0/svu"
+	_ "github.com/git-chglog/git-chglog/cmd/git-chglog"
 	_ "github.com/goreleaser/goreleaser"
+	_ "github.com/x-motemen/gobump/cmd/gobump"
+
+	// Generating
+	_ "golang.org/x/tools/cmd/stringer"
 )
